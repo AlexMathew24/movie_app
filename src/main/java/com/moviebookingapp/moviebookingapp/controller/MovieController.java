c0af949bb828ce62672fe6e122da5fbb4f6545b6
alex <alex224mathew@gmail.com>
2023-07-08 13:32:29 +0530

    databaseconnected

--- src/main/java/com/moviebookingapp/moviebookingapp/controller/MovieController.java
+++ src/main/java/com/moviebookingapp/moviebookingapp/controller/MovieController.java
@@ -1,2 +1,17 @@
-package com.moviebookingapp.moviebookingapp.controller;public class MovieController {
+package com.moviebookingapp.moviebookingapp.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api/v1.0/moviebooking")
+public class MovieController {
+
+    @PostMapping(value="/admin/addmovie")
+    public String addMovie(){
+        return "add contoller work";
+    }
 }
